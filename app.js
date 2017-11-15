02be28d8e860653e07a80915d79bf99e2c5fe1bb
Jong Park <maclay13531@gmail.com>
2017-11-15 13:01:04 -0500

    1on1 chat alpha pahse

--- app.js
+++ app.js
@@ -123,17 +123,33 @@ app.use(function(err, req, res, next) {
   res.render('error');
 });
 
-app.io.on('connect', function(socket){  
+app.io.on('connect', function(socket){ 
+    app.io.emit('newSocket', {
+        socketID: socket.id
+    })
     console.log('A USER CONNECTED TO THE SERVER VIA A SOCKET');
     socket.on('messageToServer', function(msg){
     	// var name = socket.request.session.fname;
     	// console.log(socket.request.session.fname);
         console.log(msg);
-        app.io.emit('messageToClient', msg)
+        console.log(socket.id);
+        app.io.emit('messageToClient', {
+            msg: msg,
+            clientId: socket.id
+        })
+        // socket: socket.id
+
     });
     socket.on('modalMessageToServer', function(msg){
         console.log(msg);
         var idToCheck = msg.infoSelect;
+        var socketInfoSelect = msg.socketInfoSelect;
+        var initClientID = msg.initSocketID.socketID;
+        console.log("--------------")
+        console.log(socketInfoSelect);
+        console.log(initClientID);
+        console.log("--------------")
+
         var clickedUserInfo = {};  
         var checkQuery = "select * from users where id = ?;";
         connection.query(checkQuery,[idToCheck],(error,results)=>{
@@ -154,7 +170,11 @@ app.io.on('connect', function(socket){
                     clickedUserInfo.phone = row.phone;
                 }
             }
-            app.io.emit('modalMessageToClient', clickedUserInfo)
+            app.io.emit('modalMessageToClient', {
+                clickedUserInfo: clickedUserInfo,
+                socketInfoSelect: socketInfoSelect,
+                initClientID: initClientID,
+            })
                 // fname: clickedUserInfo.fname,
                 // lname: clickedUserInfo.lname,
                 // email: clickedUserInfo.email,
@@ -162,8 +182,15 @@ app.io.on('connect', function(socket){
         });
     });
     socket.on('soloMessageToServer', function(msg){
+        var socketInfoSelect = msg.socketInfoSelect;
+        var initClientID = msg.initSocketID.socketID;        
         console.log(msg + "one to one chat");
-        app.io.emit('soloMessageToClient', msg)
+        app.io.emit('soloMessageToClient', {
+            msg: msg,
+            clientId: socket.id,
+            socketInfoSelect: socketInfoSelect,
+            initClientID: initClientID
+        })
     });
     // socket.on('nameToServer', function(name){
     //     console.log(name);
